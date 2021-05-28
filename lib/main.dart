ea0bc5044cccdaa40a6afed3281501a88b812181
Daniel-Fonseca-da-Silva <developer-web@programmer.net>
2021-05-28 20:05:42 -0300

    Adicionado arquivo resultado

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import './questao.dart';
 import './resposta.dart';
+import './resultado.dart';
 
 main() => runApp(new PerguntaApp()); // Inícia a aplicação
 
@@ -9,44 +10,56 @@ class _PerguntaAppState extends State<PerguntaApp> {
   // Variável privada
   var _perguntaSelecionada = 0;
 
+  // Lista privada
+  final _perguntas = [
+    {
+      'texto': 'Qual é a sua cor favorita?',
+      'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
+    },
+    {
+      'texto': 'Qual é o seu animal favorito?',
+      'respostas': ['Peixe', 'Cobra', 'Ave', 'Gato']
+    },
+    {
+      'texto': 'Qual é o seu instrutor favorito?',
+      'respostas': ['Nikolai', 'Irina', 'Pietro', 'Will']
+    }
+  ];
+
   // Metódo privado
   void _responder() {
-    setState(() {
-      _perguntaSelecionada++;
-    });
+    if (temPerguntaSelecionada) {
+      setState(() {
+        _perguntaSelecionada++;
+      });
+    }
+  }
+
+  // Metódo de validação
+  bool get temPerguntaSelecionada {
+    return _perguntaSelecionada < _perguntas.length;
   }
 
   // Metódo build
   @override // <- Sobrescrevendo o método build
   Widget build(BuildContext context) {
-    final List<Map<String, Object>> perguntas = [
-      {
-        'texto': 'Qual é a sua cor favorita?',
-        'respostas': ['Preto', 'Vermelho', 'Verde', 'Branco']
-      },
-      {
-        'texto': 'Qual é o seu animal favorito?',
-        'respostas': ['Peixe', 'Cobra', 'Ave', 'Gato']
-      },
-      {
-        'texto': 'Qual é o seu instrutor favorito?',
-        'respostas': ['Nikolai', 'Irina', 'Pietro', 'Will']
-      }
-    ];
-
-    List<String> respostas = perguntas[_perguntaSelecionada].cast() ['respostas'];
+    List<String> respostas = temPerguntaSelecionada
+        ? _perguntas[_perguntaSelecionada].cast()['respostas']
+        : [''];
 
     return new MaterialApp(
       home: Scaffold(
         appBar: AppBar(
           title: Text('Perguntas'),
         ),
-        body: Column(
-          children: <Widget>[
-            Questao(perguntas[_perguntaSelecionada] ['texto'].toString()),
-            ...respostas.map((t) => Resposta(t, _responder)).toList(),
-          ],
-        ),
+        body: temPerguntaSelecionada
+            ? Column(
+                children: <Widget>[
+                  Questao(_perguntas[_perguntaSelecionada]['texto'].toString()),
+                  ...respostas.map((t) => Resposta(t, _responder)).toList(),
+                ],
+              )
+            : Resultado(),
       ),
     );
   }
---
+++ lib/resultado.dart
@@ -0,0 +1,12 @@
+import 'package:flutter/material.dart';
+
+class Resultado extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Center(
+        child: Text(
+      'Parabéns usuário',
+      style: TextStyle(fontSize: 28),
+    ));
+  }
+}
